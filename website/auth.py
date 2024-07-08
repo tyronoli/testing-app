041c376db5f2ce0a73269f7760a90b14dc5ca71a
Tyron Adrian Oli <tyronoli07@gmail.com>
2024-07-08 09:16:58 +0800

    first commit

--- website/auth.py
+++ website/auth.py
@@ -14,6 +14,7 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
+
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
